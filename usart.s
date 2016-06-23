40d6689d694b52b278cc0151d6b80fbb1f48f892
Jordan Wright <jordan.wright@dal.ca>
2016-06-23 09:55:11 -0300

    usart_init, using wrong baud rate divisor for 9600 usart_send_packet removed, code now part of packet module

--- usart.s
+++ usart.s
@@ -6,12 +6,16 @@
 ;
 ; USART0 Receive complete vector handler
 ;
+; Receive Complete RXC0 flag (in UCSRnA) is high when unread data is present in the receive buffer.
+;
+
 vect_USART0_RX:
 
-    ; A character has been received, check the state of the timer0 module
-    
-    reti
+    ; A character has been received, place it in r16 and call packet handling routine
+    lds r16, UDR0
+    call packet_insert
 
+    reti
 
 ;
 ; USART0 Initialization
@@ -21,57 +25,58 @@ usart_init:
 
     ; Set baud rate to 9600
     ldi r16, 0
-    ldi r17, 7
+    ldi r17, 95
     sts UBRR0H, r16
     sts UBRR0L, r17
-    
+
     ; Enable transmitter & receiver, enable RX Complete interrupt
     ldi r17, (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0)
     sts UCSR0B, r17
-    
+
     ; Set frame format: 8 data bits, 1 stop bit, no parity.
     ldi r17, (1 << UCSZ01) | (1 << UCSZ00)
     sts UCSR0C, r17
-    
+
     ret
 
-;
-; USART Transmit packet routine
-;
 
-usart_send_packet:
-    ldi r16, 8                               ; 8 bytes to send
-    ldi r26, lo8(packet_buffer)        ; set index register to the packet buffer
-    ldi r27, hi8(packet_buffer)
-    
-usart_wait_until_ready:                      ; loop until Data Register Empty flag is set
-    lds r17, UCSR0A                          ; while clear, data transmission is already in progress
-    sbrs r17, UDRE0
-    rjmp usart_wait_until_ready
-    
-    dec r16
-    cpi r16, 0
-    brne usart_continue
-    ret
-    
-usart_continue:
 
-    ld r18, X+                               ; read next byte in buffer    
-    sts UDR0, r18                            ; store to USART data register (initiates transmit)
-    rjmp usart_wait_until_ready
-    
 
 ;
-; getc - block and wait for a character, result placed in r16 
+; getc - block and wait for a character, result placed in r16
 ;
 usart_getc:
-    
-    lds r16, UCSR0A    
+
+    lds r16, UCSR0A
     sbrs r16, RXC0
     rjmp usart_getc
     lds r16, UDR0
 
-    ret    
-    
-    
-    
+    ret
+
+
+;
+; putc - wait until USART available, then output raw character in r16
+;
+usart_putc:
+    push r17
+
+usart_putc_wait:
+    lds r17, UCSR0A       ; while clear, data transmission is already in progress
+    sbrs r17, UDRE0
+    rjmp usart_putc_wait
+    sts UDR0, r16         ; store to USART data register (initiates transmit)
+
+    pop r17
+    ret
+
+usart_ack:
+    ldi r16, 0x4f ; O
+    rcall usart_putc
+    ldi r16, 0x6b ; k
+    rcall usart_putc
+    ldi r16, 0x0d
+    rcall usart_putc
+    ldi r16, 0x0a
+    rcall usart_putc
+    ret
