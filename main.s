cbfc0db3cb93b497a1b9733bd248d5c22746c925
Jordan Wright <jordan.wright@dal.ca>
2016-07-06 14:04:56 -0300

    fixed vector table with up-to-date datasheet. oops. also moved to usart1

--- main.s
+++ main.s
@@ -23,7 +23,7 @@ jmp vect_UNDEFINED   ; vect_TIMER0_COMPA   ; Timer/Counter0 Compare Match A
 jmp vect_UNDEFINED   ; vect_TIMER0_COMPB   ; Timer/Counter0 Compare match B
 jmp vect_TIMER0_OVF  ; vect_TIMER0_OVF     ; Timer/Counter0 Overflow
 jmp vect_UNDEFINED   ; vect_SPI_STC        ; SPI Serial Transfer Complete
-jmp vect_USART0_RX   ; vect_USART0_RX      ; USART0 Rx Complete
+jmp vect_UNDEFINED   ; vect_USART0_RX      ; USART0 Rx Complete
 jmp vect_UNDEFINED   ; vect_USART0_UDRE    ; USART0 Data Register Empty
 jmp vect_UNDEFINED   ; vect_USART0_TX      ; USART0 Tx Complete
 jmp vect_UNDEFINED   ; vect_ANALOG_COMP    ; Analog Comparator
@@ -31,6 +31,13 @@ jmp vect_UNDEFINED   ; vect_ADC            ; ADC Conversion Complete
 jmp vect_UNDEFINED   ; vect_EE_READY       ; EEPROM Ready
 jmp vect_UNDEFINED   ; vect_TWI            ; 2-wire Serial Interface
 jmp vect_UNDEFINED   ; vect_SPM_READY      ; Store Program Memory Ready
+jmp vect_USART1_RX   ; vect_USART1_RX      ; USART1 Rx Complete
+jmp vect_UNDEFINED   ; vect_USART1_UDRE    ; USART1 Data Register Empty
+jmp vect_UNDEFINED   ; vect_USART1_TX      ; USART1 Tx Complete
+jmp vect_UNDEFINED   ; vect_TIMER3_CAPT    ; Timer/Counter3 Capture Event
+jmp vect_UNDEFINED   ; vect_TIMER3_COMPA   ; Timer/Counter3 Compare Match A
+jmp vect_UNDEFINED   ; vect_TIMER3_COMPB   ; Timer/Counter3 Compare Match B
+jmp vect_UNDEFINED   ; vect_TIMER3_OVF     ; Timer/Counter3 Overflow
 
 vect_UNDEFINED:
     reti
