293b6621b0c5da91d7e65d3b6d9fe4d0e8f9f97d
Jordan Wright <jordan.wright@dal.ca>
2016-07-06 10:54:29 -0300

    saved registers in 500ms delay function

--- delay.s
+++ delay.s
@@ -1,5 +1,9 @@
 
 delay_500ms:
+    push r18
+    push r19
+    push r20
+
     ldi  r18, 38
     ldi  r19, 103
     ldi  r20, 245
@@ -11,5 +15,9 @@ delay_500ms_L1:
     dec  r18
     brne delay_500ms_L1
     nop
+
+    pop r20
+    pop r19
+    pop r18
     ret
-    
+
