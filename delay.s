e45c859c80cc692bc70509a8714b911f2871dff0
Jordan Wright <jordan.wright@dal.ca>
2016-07-07 11:05:16 -0300

    added 50ms delay routine

--- delay.s
+++ delay.s
@@ -20,3 +20,27 @@ delay_500ms_L1:
     pop r19
     pop r18
     ret
+
+
+delay_50ms:
+    push r18
+    push r19
+    push r20
+
+    ldi  r18, 4
+    ldi  r19, 190
+    ldi  r20, 124
+delay_50ms_L1:
+    dec  r20
+    brne delay_500ms_L1
+    dec  r19
+    brne delay_500ms_L1
+    dec  r18
+    brne delay_500ms_L1
+    nop
+
+    pop r20
+    pop r19
+    pop r18
+    ret
+
