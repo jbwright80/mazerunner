d63708eec62f371d071962d81ffaf9cb2b779e8e
Jordan Wright <jordan.wright@dal.ca>
2016-07-19 13:20:51 -0300

    added decode_is_for_me, and check max hopcount

--- packet-decode.s
+++ packet-decode.s
@@ -3,17 +3,31 @@
 
 .text
 
-; If this is a reset packet from Controller, return 1 in r16, 0 otherwise
-; Assuming packet is for master
-decode_is_reset:
-    lds r16, packet_buffer + 2
-    cpi r16, 10
-    brne decode_is_reset_not
-    mov r16, r1
-    ret    
-decode_is_reset_not:
-    mov r16, r0
+
+; Is the packet addressed to the master? r16=1 if true, 0 otherwise
+decode_is_for_me:
+    lds r16, packet_buffer + 1
+    cpi r16, 1
+    brne decode_is_for_me_not
     ret
+decode_is_for_me_not:
+    clr r16
+    ret
+
+
+; Has incoming packet exceeded max hop count? r16=1 if true, 0 otherwise
+decode_check_max_hopcount:
+    lds r16, packet_buffer + 6
+    subi r16, 8 ; Eight active nodes in ring
+    brsh decode_check_max_hopcount_true
+    clr r16
+    ret
+decode_check_max_hopcount_true:
+    mov r16, r1
+    ret
+
+
+
 
 ; If this is a Gate select packet from Controller, return 1 in r16, 0 otherwise
 ; Assuming packet is for master
